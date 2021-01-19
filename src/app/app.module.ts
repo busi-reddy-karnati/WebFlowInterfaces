3956f54275b1dad89c2ef7fe065556eb4b595381
reddydavinci <busisatya@gmail.com>
2021-01-19 07:20:03 +0530

    Added ProductInfo Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductinfoComponent } from './productinfo/productinfo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductinfoComponent
   ],
   imports: [
     BrowserModule,
