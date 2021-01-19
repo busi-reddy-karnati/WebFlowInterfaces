143c29f9359520c07048c2541b1120808d7a47fe
reddydavinci <busisatya@gmail.com>
2021-01-19 08:18:42 +0530

    Added Forgot Password Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductinfoComponent } from './productinfo/productinfo.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductinfoComponent
+    ProductinfoComponent,
+    ForgotPasswordComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
