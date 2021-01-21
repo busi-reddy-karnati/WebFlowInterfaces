424096244e7e05b94726dc7414d39121aca0731f
manojjanapala <manojjanapala18@gmail.com>
2021-01-21 20:08:39 +0530

    dashboardandproductlist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
