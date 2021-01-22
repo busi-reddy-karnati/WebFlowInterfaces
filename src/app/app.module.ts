4b7058fdd690a4a205f602def9ff167a7541b859
reddydavinci <busisatya@gmail.com>
2021-01-22 10:25:48 +0530

    Added Login and Register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { DashBoardComponent } from './dash-board/dash-board.component';
-import { ProductlistComponent } from './productlist/productlist.component';
-=======
 import { ProductinfoComponent } from './productinfo/productinfo.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
->>>>>>> 20eb9023da328a1e7bdbb51838358dd1e5a0e7dc
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    DashBoardComponent,
-    ProductlistComponent
-=======
     ProductinfoComponent,
     ForgotPasswordComponent,
-    ChangePasswordComponent
->>>>>>> 20eb9023da328a1e7bdbb51838358dd1e5a0e7dc
+    ChangePasswordComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
